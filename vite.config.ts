b343fe230bbea632051a08e3fe17f3936fed1363
Michael Roberts <michael@observerly.com>
2022-01-03 14:52:16 +0000

    chore: Added lib build output to vite.config.ts as well as vitest setup.

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+
+import typescript from '@rollup/plugin-typescript'
+
+import { resolve } from 'path'
+
+export default defineConfig({
+  test: {
+    // ...
+  },
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, '/src')
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: '@observerly/polaris',
+      fileName: format => `polaris.${format}.js`
+    }
+  }
+})
