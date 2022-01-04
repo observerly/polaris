31eed32da86f590f0b16da9a15b89e0d65c21317
Michael Roberts <michael@observerly.com>
2022-01-04 08:55:42 +0000

    feat: Added getNumberOfJulianCenturiesSinceEpoch1900() to epoch module.
    feat: Added getNumberOfJulianCenturiesSinceEpoch1900() to epoch module. Includes associated test suite for module export definition.

--- src/epoch/index.ts
+++ src/epoch/index.ts
@@ -3,3 +3,5 @@ export { getJulianDate } from './getJulianDate'
 export { getJulianYearInSeconds } from './getJulianYearInSeconds'
 
 export { getModifiedJulianDate } from './getModifiedJulianDate'
+
+export { getNumberOfJulianCenturiesSinceEpoch1900 } from './getNumberOfJulianCenturiesSinceEpoch1900'
