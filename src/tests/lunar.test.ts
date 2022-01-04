f496de6cda918bf4dc690cf45e3dbe9d79c12a64
Michael Roberts <michael@observerly.com>
2022-01-04 16:47:17 +0000

    feat: Added getLunarMeanElongation() to lunar module.
    feat: Added getLunarMeanElongation() to lunar module. Includes associated test suite for module export definition and expected output.

--- src/tests/lunar.test.ts
+++ src/tests/lunar.test.ts
@@ -1,6 +1,10 @@
 import { describe, expect, it, suite } from 'vitest'
 
-import { getLunarMeanLongitude, getNumberOfJulianCenturiesSinceEpoch2000 } from '../'
+import {
+  getLunarMeanElongation,
+  getLunarMeanLongitude,
+  getNumberOfJulianCenturiesSinceEpoch2000
+} from '..'
 
 // For testing we need to specify a date because most calculations are
 // differential w.r.t a time component. We set it to the date provided
@@ -11,13 +15,23 @@ export const datetime = new Date('1992-04-12T00:00:00.000+00:00')
 const T = getNumberOfJulianCenturiesSinceEpoch2000(datetime)
 
 suite('@observerly/polaris Utilities', () => {
+  describe('Lunar Mean Elongation', () => {
+    it('getLunarMeanElongation should be defined', () => {
+      expect(getLunarMeanElongation).toBeDefined()
+    })
+
+    it('getLunarMeanElongation should be', () => {
+      const D = getLunarMeanElongation(T)
+      expect(D).toBeCloseTo(113.842304)
+    })
+  })
+
   describe('Lunar Mean Longitude', () => {
     it('getLunarMeanLongitude should be defined', () => {
       expect(getLunarMeanLongitude).toBeDefined()
     })
 
     it('getLunarMeanLongitude should be', () => {
-      console.log(T)
       const L = getLunarMeanLongitude(T)
       expect(L).toBeCloseTo(134.290182)
     })
