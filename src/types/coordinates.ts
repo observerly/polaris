56c1456da03e2b6382658cf8789ea0c8a98f03f1
Michael Roberts <michael@observerly.com>
2022-01-05 14:02:36 +0000

    Added type GeocentricEclipticCoordinate to coordinates module.
    Added type GeocentricEclipticCoordinate to coordinates module.

--- src/types/coordinates.ts
+++ src/types/coordinates.ts
@@ -17,16 +17,57 @@ export type EquatorialCoordinate = {
   dec: number
 }
 
+export type GeocentricEclipticCoordinate = {
+  /**
+   *
+   * Ecliptic longitude or celestial longitude measures the angular distance of an
+   * object along the ecliptic from the primary direction. Like right ascension in
+   * the equatorial coordinate system, the primary direction (0° ecliptic longitude)
+   * points from the Earth towards the Sun at the vernal equinox of the Northern
+   * Hemisphere. Because it is a right-handed system, ecliptic longitude is measured
+   * positive eastwards in the fundamental plane (the ecliptic) from 0° to 360°.
+   * Because of axial precession, the ecliptic longitude of most "fixed stars"
+   * (referred to the equinox of date) increases by about 50.3 arcseconds per year,
+   * or 83.8 arcminutes per century, the speed of general precession. However, for
+   * stars near the ecliptic poles, the rate of change of ecliptic longitude is
+   * dominated by the slight movement of the ecliptic (that is, of the plane of
+   * the earth's orbit), so the rate of change may be anything from minus infinity
+   * to plus infinity depending on the exact position of the star.
+   *
+   */
+  λ: number
+  /**
+   *
+   * Ecliptic latitude or celestial latitude measures the angular distance of an
+   * object from the ecliptic towards the north (positive) or south (negative)
+   * ecliptic pole. For example, the north ecliptic pole has a celestial latitude
+   * of +90°. Ecliptic latitude for "fixed stars" is not affected by precession.
+   *
+   */
+  β: number
+  /**
+   *
+   * Distance is also necessary for a complete spherical position. Different distance
+   * units are used for different objects. Within the Solar System, astronomical
+   * units are used, and for objects near the Earth, Earth radii or kilometers are
+   * used.
+   *
+   */
+  Λ: number
+}
+
 export type GeographicCoordinate = {
   /**
    *
-   * the angular distance of a place east or west of the Greenwich meridian (in unit degrees)
+   * the angular distance of a place east or west of the Greenwich meridian (in unit
+   * degrees)
    *
    */
   longitude: number
   /**
    *
-   * the angular distance of a place north or south of the earth's equator (in unit degrees)
+   * the angular distance of a place north or south of the earth's equator (in unit
+   * degrees)
    *
    */
   latitude: number
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 export type {
   EquatorialCoordinate,
+  GeocentricEclipticCoordinate,
   GeographicCoordinate,
   HorizontalCoordinate
 } from './coordinates'
