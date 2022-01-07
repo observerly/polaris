52cfff5f1efc2f2bb97f221be07037454abeab46
Michael Roberts <michael@observerly.com>
2022-01-07 11:09:31 +0000

    feat: Added getEclipticObliquity() to astrometry module.
    feat: Added getEclipticObliquity() to astrometry module. Includes associated test suite for module export definition and expected output.

--- src/astrometry/index.ts
+++ src/astrometry/index.ts
@@ -1,3 +1,5 @@
+export { getEclipticObliquity } from './getEclipticObliquity'
+
 export { getEclipticObliquityEpoch2000 } from './getEclipticObliquityEpoch2000'
 
 export { getHourAngle } from './getHourAngle'
