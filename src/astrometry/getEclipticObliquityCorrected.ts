52a5e45608032e22da48ae13627635080e2e32c3
Michael Roberts <michael@observerly.com>
2022-01-07 11:53:08 +0000

    fix: Typo in method call name.

--- src/astrometry/getEclipticObliquityCorrected.ts
+++ src/astrometry/getEclipticObliquityCorrected.ts
@@ -8,7 +8,7 @@ import { convertDegreeToRadian } from '../utilities'
  * @param Ω (of type number) the nutation in the axis of rotation
  * @returns the correction to the obliquity of the ecliptic due to nutation effects
  */
-export const getSolarEclipticObliquityCorrected = (O: number, Ω: number): number => {
+export const getEclipticObliquityCorrected = (O: number, Ω: number): number => {
   Ω = convertDegreeToRadian(Ω)
 
   return O + 0.00256 * Math.cos(Ω)
