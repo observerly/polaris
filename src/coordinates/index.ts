66da2a99b9d93fab258cdbcc14b955adca6aa696
Michael Roberts <michael@observerly.com>
2022-01-12 17:04:03 +0000

    feat: Added precessEquatorialCoordinate() to coordinates module.
    feat: Added precessEquatorialCoordinate() to coordinates module. Includes associated test suite for module export definition and expected output.

--- src/coordinates/index.ts
+++ src/coordinates/index.ts
@@ -3,3 +3,5 @@ export { convertEclipticToEquatorial } from './convertEclipticToEquatorial'
 export { convertEquatorialToHorizontal } from './convertEquatorialToHorizontal'
 
 export { convertHorizontalToEquatorial } from './convertHorizontalToEquatorial'
+
+export { precessEquatorialCoordinate } from './precessEquatorialCoordinate'
