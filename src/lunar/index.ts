2cef95e065e13cea1cfadf2823c3083bea1abf01
Michael Roberts <michael@observerly.com>
2022-01-19 15:35:21 +0000

    feat: Added getMoon() to lunar module.
    feat: Added getMoon() to lunar module. Added new Moon type to listed exports. Includes associated test suite for module export definition and expected output.

--- src/lunar/index.ts
+++ src/lunar/index.ts
@@ -16,6 +16,8 @@ export { getLunarPhase } from './getLunarPhase'
 
 export { getLunarPhaseAngle } from './getLunarPhaseAngle'
 
+export { getMoon } from './getMoon'
+
 export { MEEUS_TABLE_47A } from './tables/meeus/47a'
 
 export { MEEUS_TABLE_47B } from './tables/meeus/47b'
