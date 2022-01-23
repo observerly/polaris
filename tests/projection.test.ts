d6d22fab667a561b28b4a6a2964bb115e5ee4f1d
Michael Roberts <michael@observerly.com>
2022-01-23 14:36:48 +0000

    feat: Added convertStereoToHorizontal() to projection module.
    feat: Added convertStereoToHorizontal() to projection module. Includes associated test suite for module export definition and expected output.

--- tests/projection.test.ts
+++ tests/projection.test.ts
@@ -1,11 +1,17 @@
 import { describe, expect, it, suite } from 'vitest'
 
-import { convertHorizontalToStereo } from '../src'
+import { convertHorizontalToStereo, convertStereoToHorizontal } from '../src'
 
 suite('@observerly/polaris Projection', () => {
   describe('Horizontal to Stereo conversion', () => {
     it('convertHorizontalToStereo should be defined', () => {
       expect(convertHorizontalToStereo).toBeDefined()
+    })
+  })
+
+  describe('Stereo to Horizontal conversion', () => {
+    it('convertStereoToHorizontal should be defined', () => {
+      expect(convertStereoToHorizontal).toBeDefined()
     })
   })
 })
