b2dfdb575a9f2656c0b612ec277712bdcadbe41f
Michael J. Roberts <michael@observerly.com>
2022-03-25 17:00:03 +0100

    fix: Added improved time accuracy to getCurrentYearEnd().

--- src/epoch/getCurrentYearEnd.ts
+++ src/epoch/getCurrentYearEnd.ts
@@ -5,5 +5,5 @@
  * @returns December 31st relative to the specified datetime
  */
 export const getCurrentYearEnd = (datetime: Date) => {
-  return new Date(new Date(datetime).getFullYear(), 11, 31)
+  return new Date(new Date(datetime).getFullYear(), 11, 31, 0, 0, 0, 0)
 }
