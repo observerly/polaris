d7402edaa40ddfe9901fe1fd9ed566188fdb1b76
Michael J. Roberts <michael@observerly.com>
2022-12-23 14:22:49 +0000

    feat: Added { horizontalCoordinateFocus, ...focus } to convertHorizontalToStereo().
    feat: Added { horizontalCoordinateFocus, ...focus } to convertHorizontalToStereo().

--- src/projection/convertHorizontalToStereo.ts
+++ src/projection/convertHorizontalToStereo.ts
@@ -7,22 +7,34 @@ import { convertDegreeToRadian } from '../utilities'
  * convertHorizontalToStereo()
  *
  * @param horizontalCoordinate representing the { alt, az } position of a particular observed body.
+ * @param horizontalFocusCoordinate representing the { alt, az } position of the "center" of the projected "canvas".
+ * @param focus (of type number) representing the focal length of the stereographic projection.
  * @param width (of type number) representing the width of the projected "canvas"
  * @param height (of type number) representing the height of the projected "canvas"
  * @returns the Cartesian Coordinate { x, y } conversion to the stereographic projection
  */
 export const convertHorizontalToStereo = (
   horizontalCoordinate: HorizontalCoordinate,
+  horizontalFocusCoordinate: HorizontalCoordinate,
+  focus: number,
   width: number,
   height: number
 ): CartesianCoordinate => {
   const { az, alt } = horizontalCoordinate
 
-  const f = 0.42
+  const { alt: altCentroid } = horizontalFocusCoordinate
 
-  const sinalt1 = 0
+  // We're dealing with a perfect sphere of radius 1:
+  const R = 1
 
-  const cosalt1 = 1
+  // The focal length of the projection:
+  const f = focus
+
+  // The following is 0 for the polar "zenith", i.e. (0, 0):
+  const sinalt1 = Math.sin(convertDegreeToRadian(altCentroid))
+
+  // The following is 1 for the poalr "zenith", i.e. (0, 0):
+  const cosalt1 = Math.cos(convertDegreeToRadian(altCentroid))
 
   const cosaz = Math.cos(convertDegreeToRadian(az) - Math.PI)
 
@@ -32,7 +44,7 @@ export const convertHorizontalToStereo = (
 
   const cosalt = Math.cos(convertDegreeToRadian(alt))
 
-  const k = 2 / (1 + sinalt1 * sinalt + cosalt1 * cosalt * cosaz)
+  const k = (2 * R) / (1 + sinalt1 * sinalt + cosalt1 * cosalt * cosaz)
 
   const x = width / 2 + f * k * height * cosalt * sinaz
 
