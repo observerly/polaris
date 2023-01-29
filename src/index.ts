95a58b62039119bb1e8588bc9dd0e72a92c523e6
Michael J. Roberts <michael@observerly.com>
2023-01-29 12:19:42 +0000

    feat: Added SECONDS_ related constants to time module.
    feat: Added SECONDS_ related constants to time module.

--- src/index.ts
+++ src/index.ts
@@ -105,6 +105,10 @@ export { getEarthEccentricity, getEarthObliquity } from './terra'
 
 /*****************************************************************************************************************/
 
+export { SECONDS_IN_DAY, SECONDS_IN_HOUR, SECONDS_IN_MINUTE } from './time'
+
+/*****************************************************************************************************************/
+
 export type {
   Body,
   CartesianCoordinate,
---
+++ src/time/index.ts
@@ -0,0 +1,1 @@
+export { SECONDS_IN_DAY, SECONDS_IN_HOUR, SECONDS_IN_MINUTE } from './constants'
