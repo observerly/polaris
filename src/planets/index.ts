96bf25e7c222aea3d67810e73f0ae142b7cd5c51
Michael J. Roberts <michael@observerly.com>
2023-04-04 17:17:29 +0100

    feat: Added getPlanetaryHeliocentricEclipticCoordinate() to planets module.
    feat: Added getPlanetaryHeliocentricEclipticCoordinate() to planets module. Includes associated test suite for module export definition and expected output.

--- src/planets/index.ts
+++ src/planets/index.ts
@@ -1,5 +1,7 @@
 export { getPlanetaryEquationOfCenter } from './getPlanetaryEquationOfCenter'
 
+export { getPlanetaryHeliocentricEclipticCoordinate } from './getPlanetaryHeliocentricEclipticCoordinate'
+
 export { getPlanetaryMeanAnomaly } from './getPlanetaryMeanAnomaly'
 
 export { getPlanetaryTrueAnomaly } from './getPlanetaryTrueAnomaly'
